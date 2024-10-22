1298d999087d8d59cfcfa4dc9b990e6d752c8525
Tetsh Macbook <tetshmacbook@Tetshs-MBP.fritz.box>
2024-10-22 18:00:24 +0200

    created the sidebar and navbar in the dashboard page

---
+++ backend/components/Sidebar.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="w-[200px]  transition-all bg-indigo-700 text-white py-4 px-2">
+      <NuxtLink to="/dashboard"
+                   class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+        <span class="mr-2 text-gray-300">
+          <HomeIcon class="w-5"/>
+        </span>
+        <span class="text-xs">
+          Dashboard
+        </span>
+      </NuxtLink>
+      <NuxtLink to="products"
+                   class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+        <span class="mr-2 text-gray-300">
+          <ViewListIcon class="w-5"/>
+        </span>
+        <span class="text-xs">
+          Products
+        </span>
+      </NuxtLink>
+      <NuxtLink to="/users"
+                   class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+        <span class="mr-2 text-gray-300">
+          <UsersIcon class="w-5"/>
+        </span>
+        <span class="text-xs">
+          Users
+        </span>
+      </NuxtLink>
+      <NuxtLink to="/users"
+                   class="flex items-center p-2 rounded transition-colors hover:bg-black/30">
+        <span class="mr-2 text-gray-300">
+          <ChartBarIcon class="w-5"/>
+        </span>
+        <span class="text-xs">
+          Reports
+        </span>
+      </NuxtLink>
+    </div>
+  </template>
+  
+  <script setup>
+    import {HomeIcon, UsersIcon, ViewListIcon, ChartBarIcon} from '@heroicons/vue/outline'
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
--- backend/pages/dashboard.vue
+++ backend/pages/dashboard.vue
@@ -1,13 +1,33 @@
 <template>
-    <div>
-        Dashboard
+    <div class="h-screen bg-gray-200 flex">
+      <!--    Sidebar-->
+      <Sidebar :class="{'-ml-[200px]': !sidebarOpened}"/>
+      <!--/    Sidebar-->
+  
+      <div class="flex-1">
+        <Navbar @toggle-sidebar="toggleSidebar"></Navbar>
+        <!--      Content-->
+        <main class="p-6">
+          content goes here
+        </main>
+        <!--      Content-->
+      </div>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-
-</style>
+  </template>
+  
+  <script setup>
+  const {title} = defineProps({
+    title: String
+  })
+  
+  const sidebarOpened = ref(true);
+  
+  function toggleSidebar() {
+    sidebarOpened.value = !sidebarOpened.value
+  }
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
