21fa2fb09acc35bf3d6648a850e8edf6980fd388
Tetsh Macbook <tetshmacbook@MacBookPro.fritz.box>
2024-11-15 16:45:22 +0100

    feat: create the product page with pagination and searching

--- backend/layouts/default.vue
+++ backend/layouts/default.vue
@@ -1,8 +1,54 @@
-<template>
+<!-- <template>
     <html class="h-full bg-white">
       <body class="h-full">
         <NuxtPage />
       </body>
     </html>
+  </template> -->
+
+  <template>
+    <div class="h-screen bg-gray-200 flex">
+      <!--    Sidebar-->
+      <Sidebar :class="{'-ml-[200px]': !sidebarOpened}"/>
+      <!--/    Sidebar-->
+  
+      <div class="flex-1">
+        <Navbar @toggle-sidebar="toggleSidebar"></Navbar>
+        <!--      Content-->
+        <main class="p-6">
+          <NuxtPage />
+        </main>
+        
+      </div>
+    </div>
   </template>
+  
+  <script setup>
+  const {title} = defineProps({
+    title: String
+  })
+  
+  const sidebarOpened = ref(true);
+  
+  function toggleSidebar() {
+    sidebarOpened.value = !sidebarOpened.value
+  }
+
+  function updateSidebarState() {
+  sidebarOpened.value = window.outerWidth > 768;
+}
+
+onMounted(() => {
+  updateSidebarState();
+  window.addEventListener('resize', updateSidebarState)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateSidebarState)
+})
+  </script>
+  
+  <style scoped>
+  
+  </style>
   
--- backend/src/store/index.js
+++ backend/src/store/index.js
@@ -1,12 +1,90 @@
 import { createStore } from "vuex";
+import axiosClient from "./axios";
 
-const store = createStore( {
-    state: {
-        test: '1234'
+const store = createStore({
+  state: {
+    user: {
+      data: {},
+      // token: sessionStorage.getItem('TOKEN'),
+      token: process.client ? sessionStorage.getItem('TOKEN') : null,
     },
-    getters: {},
-    actions: {},
-    mutations: {}
-})
+    products: {
+      loading: false,
+      data: [],
+      links: [],
+      page: 1,
+      limit: 10,
+      from: null,
+      to: null,
+      total: null,
+    },
+  },
+  getters: {},
+  actions: {
+    getUser({ commit }, data) {
+      return axiosClient.get('/user', data)
+        .then(({ data }) => {
+          commit('setUser', data);
+          return data;
+        });
+    },
+    login({ commit }, data) {
+      return axiosClient.post('/login', data)
+        .then(({ data }) => {
+          commit('setUser', data.user);
+          commit('setToken', data.token);
+          return data;
+        });
+    },
+    logout({ commit }) {
+      return axiosClient.post('/logout')
+        .then((response) => {
+          commit('setToken', null);
+          return response;
+        });
+    },
+    getProducts({ commit }, { url = null, search, perPage }) {
+      commit('setProducts', [true]);
+      url = url || '/products';
+      return axiosClient.get(url, {
+        params: { search, per_page: perPage }
+      })
+        .then((response) => {
+          commit('setProducts', [false, response.data]);
+        })
+        .catch(() => {
+          commit('setProducts', [false]);
+        });
+    },
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user.data = user;
+    },
+    setToken(state, token) {
+      state.user.token = token;
+      if (token) {
+        sessionStorage.setItem('TOKEN', token);
+      } else {
+        sessionStorage.removeItem('TOKEN');
+      }
+    },
+    setProducts(state, [loading, data = null]) {
+      if (data) {
+        state.products = {
+          ...state.products,
+          data: data.data,
+          links: data.meta?.links,
+          page: data.meta.current_page,
+          limit: data.meta.per_page,
+          from: data.meta.from,
+          to: data.meta.to,
+          total: data.meta.total,
+        };
+      }
+      state.products.loading = loading;
+    },
+  },
+});
 
 export default store;
